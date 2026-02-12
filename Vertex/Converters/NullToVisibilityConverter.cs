c548a64e47f8c4554f21068e77016d5e840f9160
Alevi <rodriguezalevi@gmail.com>
2026-02-12 16:12:11 -0600

    Configuraciones

---
+++ Vertex/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Vertex.App.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value == null ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
